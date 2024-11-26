5b04252c666aa236a7ff3549111db426a9270148
Wassim Daoudi Idrissi <wdaoudi-@made-f0Dr10s7.clusters.42paris.fr>
2024-11-26 16:49:24 +0100

    ajout de export presque pret a l emploi pour pull a la maison

--- src/builtin/export.c
+++ src/builtin/export.c
@@ -6,7 +6,7 @@
 /*   By: wdaoudi- <wdaoudi-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 15:02:29 by wdaoudi-          #+#    #+#             */
-/*   Updated: 2024/11/25 18:46:47 by wdaoudi-         ###   ########.fr       */
+/*   Updated: 2024/11/26 15:34:07 by wdaoudi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,25 @@
 int	ft_export(t_shell *shell)
 {
 	char	*looking_for;
-    t_exec *current;
-    
-    // printf("entrer dans la boucle export\n");
+	t_exec	*current;
+	int i;
+	
+	i = 1;
+	printf("entrer dans la boucle export\n");
 	current = shell->exec_line->first;
 	if (!current->cmd[0] || !current->cmd[1])
-			return (printf("encore moins a gratter"), 1);
-	looking_for = shell->exec_line->first->cmd[1];
+		return (printf("encore moins a gratter"), 1);
+	// censer afficheer les variable exporte
+	while (shell->exec_line->first->cmd[i])
+	{
+	looking_for = shell->exec_line->first->cmd[i];
 	if (!looking_for)
 		return (printf("rien a gratter\n"), 1); // gerer l erreur
-	if(is_name_ok(looking_for,shell) == 1)
+	if (is_name_ok(looking_for, shell) == 1)
 		return (printf("%d\n", is_name_ok(looking_for, shell)), 0);
-    printf("parfait\n");
+	i++;
+	}
+	printf("parfait\n");
 	return (0);
 }
 
@@ -35,14 +42,11 @@ int	is_name_ok(char *str, t_shell *shell)
 	int	i;
 
 	i = 0;
-	// if (str[i] == ' ')
-	// 	return (printf("%s: export '%s': not a valid identifier\n",
-	// 			shell->shell_name, str), 1); // gerer l erreur
 	if ((str[i] != '_' && !ft_isalpha(str[i])) || str[i] == '=')
 		return (printf("%s: export '%s': not a valid identifier\n",
 				shell->shell_name, str), 1); // gerer l erreur
 	i++;
-	while (str[i]  && str[i] != '=')
+	while (str[i] && str[i] != '=')
 	{
 		if ((str[i] != '_' && !ft_isalpha(str[i]) && !ft_isalnum(str[i])
 				&& str[i] != '='))
@@ -51,7 +55,8 @@ int	is_name_ok(char *str, t_shell *shell)
 		i++;
 	}
 	return (0);
-} // rependre dedans et continuer les verification avant de modifier sur la liste chaionee
+}
+// rependre dedans et continuer les verification avant de modifier sur la liste chaionee
 
 /*
 if first caractere is not a _ or a letter error:
